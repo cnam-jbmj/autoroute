221df50f944af8789d05d423564d759d39e93c1d
Josslin Varon <josslinvaron@gmail.com>
2019-08-17 13:19:11 +0200

    Rename package

---
+++ src/app/VueSaisie.java
@@ -0,0 +1,31 @@
+package app;
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class VueSaisie {
+
+    public FormAutoroute lire() {
+    	Scanner scanner = new Scanner(System.in);
+    	
+        System.out.print("Nombre de voitures : ");
+        int nb_v = scanner.nextInt();
+        System.out.print("Nombre de caisses : ");
+        int nb_c = scanner.nextInt();
+        System.out.print("Kilométrage min : ");
+        int min = scanner.nextInt();
+        System.out.print("Kilométrage max : ");
+        int max = scanner.nextInt();
+        System.out.print("Vitesse moyenne des véhicules : ");
+        int vitesse = scanner.nextInt();
+        
+        scanner.close();
+        
+        FormAutoroute f = new FormAutoroute(nb_v, nb_c, min, max, vitesse);
+        return f;
+    }
+}
